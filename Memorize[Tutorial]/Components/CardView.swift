a14882ebc92ba9e19e6ef5327f3330ff51420cf6
AjaXium <aymen.mzpro@gmail.com>
2024-10-03 20:58:10 +0100

    a

--- Memorize[Tutorial]/Components/CardView.swift
+++ Memorize[Tutorial]/Components/CardView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct CardView: View {
+    var isFaceUp: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: 20)
+                    .strokeBorder(lineWidth: 2)
+                Text("😄")
+                    .font(.system(size: 40))
+            }
+            else {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.orange)
+                RoundedRectangle(cornerRadius: 20)
+                    .strokeBorder(lineWidth: 2)
+            }
+        }
+        .font(.system(size: 40))
+        .foregroundStyle(Color.orange)
+        .imageScale(.small)
+        .padding()
     }
 }
 
--- Memorize[Tutorial]/ContentView.swift
+++ Memorize[Tutorial]/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack(spacing: -20){
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
-        .padding()
     }
 }
 
