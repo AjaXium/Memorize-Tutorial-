b0df4c591884a4e5c9d2d92dda916b106d2abdd0
AjaXium <aymen.mzpro@gmail.com>
2024-10-03 16:15:03 +0100

    Initial Commit

---
+++ Memorize[Tutorial]/Memorize_Tutorial_App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_Tutorial_App.swift
+//  Memorize[Tutorial]
+//
+//  Created by Aymane Maizi on 3/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_Tutorial_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
